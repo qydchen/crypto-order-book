9142988e0aa59bae66a85de1612404f38f06e7fe
David Chen <qydchen@gmail.com>
2020-11-19 20:47:21 -0500

    start spring websocket with lombok project

---
+++ src/main/java/com/david/cryptoorderbook/CryptoorderbookApplication.java
@@ -0,0 +1,13 @@
+package com.david.cryptoorderbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoorderbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoorderbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/david/cryptoorderbook/CryptoorderbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.david.cryptoorderbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoorderbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
