cfade52473700f062990e82101272eafd8692d5b
qydchen <qydchen@gmail.com>
2020-11-22 21:49:32 -0500

    handle inserts properly; print asks in reverse to match order book view

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 Make sure java runtime is compatible for java 11.
 
+From the project root directory, and after maven install:
 `java -jar target/cryptoorderbook-0.0.1-SNAPSHOT.jar ETH-EUR`
 
--- src/main/java/book/OrderBook.java
+++ src/main/java/book/OrderBook.java
@@ -5,16 +5,14 @@ import models.Snapshot;
 import utils.JSONUtils;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class OrderBook {
     final int depth = 10;
+    final int buffer = 5;
     private List<List<String>> asks;
     private List<List<String>> bids;
-
     public void receiveTick(String tick) {
         String type = JSONUtils.getType(tick);
         this.handleTick(type, tick);
@@ -28,9 +26,8 @@ public class OrderBook {
             }
             case ("snapshot"): {
                 Snapshot snapshot = JSONUtils.toSnapshot(tick);
-                this.asks = snapshot.getAsks().subList(0, 15);
-                Collections.reverse(this.asks);
-                this.bids = snapshot.getBids().subList(0, 15);
+                this.asks = snapshot.getAsks().subList(0, depth + buffer);
+                this.bids = snapshot.getBids().subList(0, depth + buffer);
                 this.print();
                 break;
             }
@@ -54,35 +51,42 @@ public class OrderBook {
         System.out.print("\033[H\033[2J");
         System.out.flush();
         System.out.println(String.join(delimiter, Arrays.asList("Price  ", "  Size")));
-        this.asks.subList(0, depth).forEach(t -> System.out.println(String.join(delimiter, t)));
+        List<List<String>> asks = this.asks.subList(0, depth);
+        for (int i = asks.size() - 1; i >= 0; i--) {
+            System.out.println(String.join(delimiter, asks.get(i)));
+        }
         System.out.println("Asks ^------------v Bids");
-        this.bids.subList(0,depth).forEach(t -> System.out.println(String.join(delimiter, t)));
+        this.bids.subList(0, depth).forEach(t -> System.out.println(String.join(delimiter, t)));
     }
 
     private void update(List<String> change) {
         String side = change.get(0);
         BigDecimal changePrice = new BigDecimal(change.get(1));
         BigDecimal changeSize = new BigDecimal(change.get(2));
-        List<List<String>> book = side == "buy" ? this.bids : this.asks;
+        List<List<String>> book = side.equals("buy") ? this.bids : this.asks;
         for (int i = 0; i < book.size(); i++) {
             List<String> currentOrder = book.get(i);
             BigDecimal currentPrice = new BigDecimal(currentOrder.get(0));
             if (changePrice.compareTo(currentPrice) == 0) {
                 if (changeSize.compareTo(BigDecimal.ZERO) == 0) {
-//                    System.out.println(String.format("remove: %s", i));
                     book.remove(i);
+                    return;
                 } else {
-//                    System.out.println(String.format("set: %s", i));
                     book.set(i, change.subList(1, 3));
+                    return;
+                }
+            } else if (changeSize.compareTo(BigDecimal.ZERO) != 0) {
+                if (side.equals("buy") && changePrice.compareTo(currentPrice) == 1) {
+                    book.add(i, change.subList(1, 3));
+                    return;
+                } else if (side.equals("sell") && changePrice.compareTo(currentPrice) == -1) {
+                    book.add(i, change.subList(1, 3));
+                    return;
+                } else if (book.size() < depth + buffer) {
+                    book.add(change.subList(1, 3));
+                    return;
                 }
             }
-        }
-        // If no updates on the current book, then it must be an insertion;
-        if (changeSize.compareTo(BigDecimal.ZERO) == 0) return;
-        if (side == "buy") {
-            book.add(change.subList(1, 3));
-        } else {
-            book.add(0, change.subList(1, 3));
         }
     }
 }
