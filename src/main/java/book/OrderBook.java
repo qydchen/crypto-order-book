30a25d3d351a5ef6fc1a3600eecab0a4f3972b63
David Chen <qydchen@gmail.com>
2020-12-04 14:11:07 -0500

    update

--- src/main/java/book/OrderBook.java
+++ src/main/java/book/OrderBook.java
@@ -7,12 +7,14 @@ import utils.JSONUtils;
 import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Collections;
 
 public class OrderBook {
     final int depth = 10;
     final int buffer = 50;
     private List<List<String>> asks;
     private List<List<String>> bids;
+    private List<String> logs = Collections.emptyList();
     public void receiveTick(String tick) {
         String type = JSONUtils.getType(tick);
         switch (type) {
@@ -30,6 +32,7 @@ public class OrderBook {
                     this.update(change);
                 }
                 this.print();
+                this.resize();
                 break;
             }
             default: {
@@ -48,18 +51,22 @@ public class OrderBook {
             BigDecimal currentPrice = new BigDecimal(currentOrder.get(0));
             if (changePrice.compareTo(currentPrice) == 0) {
                 if (changeSize.compareTo(BigDecimal.ZERO) == 0) {
+                    this.logs.add(String.format("removing index: %s, currentOrder: %s", i, currentOrder));
                     book.remove(i);
                     return;
                 } else {
+                    this.logs.add(String.format("set index: %s, currentOrder: %s", i, currentOrder));
                     book.set(i, change.subList(1, 3));
                     return;
                 }
             } else if (changeSize.compareTo(BigDecimal.ZERO) != 0) {
                 if ((side.equals("buy") && changePrice.compareTo(currentPrice) == 1) ||
                         (side.equals("sell") && changePrice.compareTo(currentPrice) == -1)) {
+                    this.logs.add(String.format("add index: %s, currentOrder: %s", i, currentOrder));
                     book.add(i, change.subList(1, 3));
                     return;
                 } else if (book.size() < depth + buffer) {
+                    this.logs.add(String.format("add to end index: %s, currentOrder: %s", i, currentOrder));
                     book.add(change.subList(1, 3));
                     return;
                 }
@@ -78,5 +85,21 @@ public class OrderBook {
         }
         System.out.println("Asks ^------------v Bids");
         this.bids.subList(0, depth).forEach(t -> System.out.println(String.join(delimiter, t)));
+
+        System.out.println(String.format("asks size: %s, bids size: %s", this.asks.size(), this.bids.size()));
+        System.out.println(this.logs);
+    }
+
+    private void resize() {
+        if (this.asks.size() > 50) {
+            this.asks = this.asks.subList(0, buffer);
+        }
+        if (this.bids.size() > 50) {
+            this.bids = this.bids.subList(0, buffer);
+        }
+        int maxLogSize = 5;
+        if (this.logs.size() > maxLogSize) {
+            this.logs = this.logs.subList(0, maxLogSize - 1);
+        }
     }
 }
